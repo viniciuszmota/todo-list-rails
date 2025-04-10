1e2c368f863bb8ee021e11dc276903e4a3442e53
vinicius zamprogno <vinicius.quimica@gmail.com>
2025-04-10 17:04:21 -0300

    devise instalado e configurado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   allow_browser versions: :modern
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username ])
+  end
 end
