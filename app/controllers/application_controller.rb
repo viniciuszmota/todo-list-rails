5e313c43df9ed9dbe1d1223c2650a0d4227bdff0
vinicius zamprogno <vinicius.quimica@gmail.com>
2025-04-15 14:10:12 -0300

    modal delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  allow_browser versions: :modern
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
